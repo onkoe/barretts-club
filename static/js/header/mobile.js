910a362011e00a355b60be9b3142a6cd6cba34ce
Barrett <onkoe@protonmail.com>
2023-11-07 19:58:50 -0600

    feat: Add mobile header animations

--- static/js/header/mobile.js
+++ static/js/header/mobile.js
@@ -4,14 +4,18 @@
 // at least they can do stuff lol
 
 var nav_container = document.querySelector(".mobile-nav-container");
-nav_container.style.display = "none";
+nav_container.classList.add("collapsed", "no-transition");
+
+setTimeout(() => {
+    nav_container.classList.remove("no-transition");
+}, 10);
 
 var checkbox = document.getElementById("menu-button-checkbox");
 
 checkbox.addEventListener("change", function () {
     if (this.checked) {
-        nav_container.style.display = "flex";
+        nav_container.classList.remove("collapsed");
     } else {
-        nav_container.style.display = "none";
+        nav_container.classList.add("collapsed");
     }
 });
